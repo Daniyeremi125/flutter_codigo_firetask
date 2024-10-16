38e62615678c97cbe26a9761d2cb00f1679a3482
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-10-15 23:49:24 -0500

    abanse 25

---
+++ lib/services/my_service_firestore.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_codigo_firetask/models/task_model.dart';
+
+class MyServiceFirestore {
+  String collection;
+
+  MyServiceFirestore({required this.collection});
+
+  late final CollectionReference _collectionReference =
+      FirebaseFirestore.instance.collection(collection);
+
+  Future<String> addTask(TaskModel model) async {
+    DocumentReference documentReference =
+        await _collectionReference.add(model.toJson());
+    String id = documentReference.id;
+    return id;
+  }
+
+  Future<void> finishedTask(String taskId) async {
+    await _collectionReference.doc(taskId).update(
+      {
+        "status": false,
+      },
+    );
+  }
+
+ 
+
+}
